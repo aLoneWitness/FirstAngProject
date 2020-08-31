39d68749b1f68eef1753fbb6c8c8d6b7c1f5f63b
Sander van Dinteren <sanvandinteren@gmail.com>
2020-08-31 10:56:41 +0200

    Interceptor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpsInterceptor} from './interceptors/https.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AppComponent,
+    { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptors/https.interceptor.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class HttpsInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Todo: Un-hardcode the URL
+    const baseUrl = 'https://testapi.jarpiscloud.nl/api/v1';
+    req = req.clone({
+      url: baseUrl + req.url
+    });
+    return next.handle(req);
+  }
+}
